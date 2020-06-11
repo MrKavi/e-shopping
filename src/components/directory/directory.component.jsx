72f05246378e8f82c69084dd7fa1b95fe9b8f912
kavi gasyal <kvigasyal@gmail.com>
2020-06-12 01:31:29 +0530

    changes

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
+
 import './directory.style.scss';
 class Directory extends React.Component{
 
@@ -11,33 +12,33 @@ class Directory extends React.Component{
         this.state ={
         sections:[
             {
-              title: 'hats',
-              imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+              title: 'save',
+              imageUrl: 'https://images.unsplash.com/photo-1478004521390-655bd10c9f43?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
               id: 1,
              // linkUrl: 'shop/hats'
             },
             {
-              title: 'jackets',
-              imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+              title: 'aam',
+              imageUrl: 'https://images.unsplash.com/photo-1582655299221-2b6bff351df0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=501&q=80',
               id: 2,
              // linkUrl: 'shop/jackets'
             },
             {
-              title: 'sneakers',
-              imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+              title: 'strawberry',
+              imageUrl: 'https://images.unsplash.com/photo-1535541201873-125fb9df316b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
               id: 3,
              // linkUrl: 'shop/sneakers'
             },
             {
-              title: 'womens',
-              imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+              title: 'malta',
+              imageUrl: 'https://images.unsplash.com/photo-1550715608-35629afe8b0e?ixlib=rb-1.2.1&auto=format&fit=crop&w=386&q=80',
               size: 'large',
               id: 4,
              // linkUrl: 'shop/womens'
             },
             {
-              title: 'mens',
-              imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+              title: 'anar',
+              imageUrl: 'https://images.unsplash.com/photo-1530730459653-e0edd4c6072e?ixlib=rb-1.2.1&auto=format&fit=crop&w=891&q=80',
               size: 'large',
               id: 5,
              // linkUrl: 'shop/mens'
